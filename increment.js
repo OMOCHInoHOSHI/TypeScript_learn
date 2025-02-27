236f49754f66a917eb776c90ccf587bdf6c11018
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-02-27 21:56:04 +0900

    コンパイラの確認

---
+++ increment.js
@@ -0,0 +1,17 @@
+// // 実行方法
+// // node increment.js
+// function increment(num) {
+//     return num + 1;
+//   }
+//   console.log(increment(999));
+//   console.log(increment("999"));
+//   // 型が動的に変わる
+function increment(num) {
+    //                 ^^^^^^^^型注釈
+    return num + 1;
+}
+console.log(increment("999"));
+// tscがコンパイラによるチェック
+// その前にpowershellで実行ポリシーを変更
+// Set-ExecutionPolicy RemoteSigned // 全体
+// Set-ExecutionPolicy RemoteSigned -Scope CurrentUser  // 今のユーザ
--- increment.ts
+++ increment.ts
@@ -1,11 +1,25 @@
-// 実行方法
-// node increment.js
+// // 実行方法
+// // node increment.js
 
-function increment(num) {
+// function increment(num) {
+//     return num + 1;
+//   }
+  
+//   console.log(increment(999));
+//   console.log(increment("999"));
+
+//   // 型が動的に変わる
+
+function increment(num: number) {
+  //                 ^^^^^^^^型注釈
     return num + 1;
   }
-  
-  console.log(increment(999));
+   
   console.log(increment("999"));
 
-  // 型が動的に変わる
+  // tscがコンパイラによるチェック
+  // tsc increment.ts
+
+  // その前にpowershellで実行ポリシーを変更
+  // Set-ExecutionPolicy RemoteSigned // 全体
+  // Set-ExecutionPolicy RemoteSigned -Scope CurrentUser  // 今のユーザ
